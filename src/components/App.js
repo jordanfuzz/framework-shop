a0dc5ed624e1d774ac17634b81c9bb62eac468a9
Jordan Cooper <jordan@cooperplanet.com>
2017-06-19 15:58:52 -0600

    Add navigation and checkout with routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import "./App.css";
 
+
 import Nav from "./Nav/Nav";
+import router from './../router'
 
 export function App( { children } ) {
 	return (
 		<div className="app">
 			<Nav />
+			{ router }
 		</div>
 	);
 }
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import ThankYou from '../../framework-shop/src/components/ThankYou/ThankYou'
+
+
+
+import Landing from './components/Landing/Landing'
+import Shop from './components/Shop/Shop'
+import Details from './components/Details/Details'
+import Cart from './components/Cart/Cart'
+
+export default (
+
+    <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/shop" component={Shop} />
+        <Route path="/details/:name" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/thank-you" component={ThankYou} />
+    </Switch>
+
+)
